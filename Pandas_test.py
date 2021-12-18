e8f86f3a40d3621d683f0016ea6060fce2be234e
UstasAlex999 <igor.t9999@gmail.com>
2021-12-18 13:45:14 +0300

    primary and foreign key concept is understood

--- Pandas_test.py
+++ Pandas_test.py
@@ -1,128 +1,56 @@
-#import pandas as pd
-#from pandas import DataFrame
 import requests
 import psycopg2
 
 
+#This section provides credentials for AWS Database
 DB_HOST = 'ls-8ac1ef06f8c0b721d61a89b671a2e10c40a7050f.cpsgene13aqe.eu-central-1.rds.amazonaws.com'
 DB_NAME =  'Stocks'
 DB_USER = 'dbmasteruser'
 DB_PASS = 'RH6t.8|Ssd[g0y)jSA+c,)UP,V*<7^ZV'
 
 Ticker ='IBM'
-date = '2017-10-15'
-currency = 'usd'
-
-
-conn = psycopg2.connect(dbname=DB_NAME, user=DB_USER, password=DB_PASS,host=DB_HOST)
-
-
-
-
-
 
 api_key='8JB4V49M6EBPR459'
 
+
+url0 = 'https://www.alphavantage.co/query?function=OVERVIEW&symbol='+ str(Ticker)+'&apikey=8JB4V49M6EBPR459'
+r0 = requests.get(url0)
+DataMain = r0.json()
+#print(type(DataMain))
+#for key, value in DataMain.items():
+#print(len(DataMain))
+
+
 url = 'https://www.alphavantage.co/query?function=INCOME_STATEMENT&symbol='+ str(Ticker)+'&apikey='+str(api_key)
-#print(url)
-
-
 r = requests.get(url)
-
-
-
 DataIncomeStatement = r.json()
 
-zz = len(DataIncomeStatement['annualReports'])
-
-str_ =''
-
-listOfnumbers = []
-
-#print(type(DataIncomeStatement['annualReports'][0]))
-print(len(DataIncomeStatement['annualReports'][0]))
-for key, value in DataIncomeStatement['annualReports'][0].items():
-#    print(key,value)
-    str_ += key
-    str_ +=','
-    listOfnumbers.append(value)
-        
 
 
-str2_ =''
-
-for i in range (len(listOfnumbers)):
-    str2_ += 'listOfnumbers['+str(i)+']'
-    str2_ += ','
-
-
-
-
-
-finalString = str_[:-1]
-
-finalString2 = str2_[:-1]
-
-#print(finalString)
-print('          ')
-#print(finalString2)
-
-
-
+conn = psycopg2.connect(dbname=DB_NAME, user=DB_USER, password=DB_PASS,host=DB_HOST)
 cur = conn.cursor()
 
-#cur.execute("CREATE TABLE main (id SERIAL PRIMARY KEY, title VARCHAR);")
-
-#test = "INSERT INTO income_statement ("+finalString+") VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)",()
-
-print(DataIncomeStatement['annualReports'][0]['grossProfit'])
-print(float(DataIncomeStatement['annualReports'][0]['grossProfit']))
-print(int(DataIncomeStatement['annualReports'][0]['grossProfit']))
-
-print(len(DataIncomeStatement['annualReports']))
-
+#This loop provides cleaning data from 'None'. When there is no data provided alphavantage API returns None. We change all the Nones to 0
 for i in range (len(DataIncomeStatement['annualReports'])):
-
-    print(DataIncomeStatement['annualReports'][i])
-
     for key, value in DataIncomeStatement['annualReports'][i].items():
-        
         if value == 'None':
-            print(key, value)
             DataIncomeStatement['annualReports'][i].update({key:0})
 
-            print('Change is added to',key, value)
 
+cur.execute("INSERT INTO list_of_companies (Symbol,Description) VALUES(%s,%s)", (DataMain['Symbol'],DataMain['Description']))
 
-print(DataIncomeStatement['annualReports'][0]['investmentIncomeNet'])
-
-
-#cur.execute("INSERT INTO income_statement (ticker,fiscalDateEnding,reportedCurrency,grossProfit,totalRevenue,costOfRevenue,costofGoodsAndServicesSold,operatingIncome,sellingGeneralAndAdministrative,researchAndDevelopment,operatingExpenses,investmentIncomeNet,netInterestIncome,interestIncome,nonInterestIncome,otherNonOperatingIncome,depreciation,depreciationAndAmortization,incomeBeforeTax,incomeTaxExpense,interestAndDebtExpense,netIncomeFromContinuingOperations,comprehensiveIncomeNetOfTax,ebit,ebitda,netIncome) VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)",(Ticker,DataIncomeStatement['annualReports'][0]['fiscalDateEnding'],DataIncomeStatement['annualReports'][0]['reportedCurrency'],float(DataIncomeStatement['annualReports'][0]['grossProfit'])float(DataIncomeStatement['annualReports'][0]['totalRevenue'])))
-
+#This loop is for writing anual data from the company Income Statement to database table Income Statement
 i=0
 for i in range (len(DataIncomeStatement['annualReports'])):
     cur.execute("INSERT INTO income_statement (ticker,fiscalDateEnding,reportedCurrency,grossProfit,totalRevenue,costOfRevenue,costofGoodsAndServicesSold,operatingIncome,sellingGeneralAndAdministrative,researchAndDevelopment,operatingExpenses,investmentIncomeNet,netInterestIncome,interestIncome,interestExpense,nonInterestIncome,otherNonOperatingIncome,depreciation,depreciationAndAmortization,incomeBeforeTax,incomeTaxExpense,interestAndDebtExpense,netIncomeFromContinuingOperations,comprehensiveIncomeNetOfTax,ebit,ebitda,netIncome) VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)",(Ticker,DataIncomeStatement['annualReports'][i]['fiscalDateEnding'],DataIncomeStatement['annualReports'][i]['reportedCurrency'],float(DataIncomeStatement['annualReports'][i]['grossProfit']),float(DataIncomeStatement['annualReports'][i]['totalRevenue']),float(DataIncomeStatement['annualReports'][i]['costOfRevenue']),float(DataIncomeStatement['annualReports'][i]['costofGoodsAndServicesSold']),float(DataIncomeStatement['annualReports'][i]['operatingIncome']),float(DataIncomeStatement['annualReports'][i]['sellingGeneralAndAdministrative']),float(DataIncomeStatement['annualReports'][i]['researchAndDevelopment']),float(DataIncomeStatement['annualReports'][i]['operatingExpenses']),float(DataIncomeStatement['annualReports'][i]['investmentIncomeNet']),float(DataIncomeStatement['annualReports'][i]['netInterestIncome']),float(DataIncomeStatement['annualReports'][i]['interestIncome']),float(DataIncomeStatement['annualReports'][i]['interestExpense']),float(DataIncomeStatement['annualReports'][i]['nonInterestIncome']),float(DataIncomeStatement['annualReports'][i]['otherNonOperatingIncome']),float(DataIncomeStatement['annualReports'][i]['depreciation']),float(DataIncomeStatement['annualReports'][i]['depreciationAndAmortization']),float(DataIncomeStatement['annualReports'][i]['incomeBeforeTax']),float(DataIncomeStatement['annualReports'][i]['incomeTaxExpense']),float(DataIncomeStatement['annualReports'][i]['interestAndDebtExpense']),float(DataIncomeStatement['annualReports'][i]['netIncomeFromContinuingOperations']),float(DataIncomeStatement['annualReports'][i]['comprehensiveIncomeNetOfTax']),float(DataIncomeStatement['annualReports'][i]['ebit']),float(DataIncomeStatement['annualReports'][i]['ebitda']),float(DataIncomeStatement['annualReports'][i]['netIncome'])))
 
 
-    
 
-#cur.execute(test)
 
-#cur.execute("SELECT * FROM income_statement;")
+#This string is for writing data from Overvier of the company to database table General_Information
+cur.execute("INSERT INTO General_Information (Symbol,AssetType,Name,Description,CIK,Exchange,Currency,Country,Sector,Industry,Address,FiscalYearEnd,LatestQuarter,MarketCapitalization,EBITDA,PERatio,PEGRatio,BookValue,DividendPerShare,DividendYield,EPS,RevenuePerShareTTM,ProfitMargin,OperatingMarginTTM,ReturnOnAssetsTTM,ReturnOnEquityTTM,RevenueTTM,GrossProfitTTM,DilutedEPSTTM,QuarterlyEarningsGrowthYOY,QuarterlyRevenueGrowthYOY,AnalystTargetPrice,TrailingPE,ForwardPE,PriceToSalesRatioTTM,PriceToBookRatio,EVToRevenue,EVToEBITDA,Beta,_52WeekHigh,_52WeekLow,_50DayMovingAverage,_200DayMovingAverage,SharesOutstanding,DividendDate,ExDividendDate) VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)", (DataMain['Symbol'],DataMain['AssetType'],DataMain['Name'],DataMain['Description'],DataMain['CIK'],DataMain['Exchange'],DataMain['Currency'],DataMain['Country'],DataMain['Sector'],DataMain['Industry'],DataMain['Address'],DataMain['FiscalYearEnd'],DataMain['LatestQuarter'],DataMain['MarketCapitalization'],DataMain['EBITDA'],DataMain['PERatio'],DataMain['PEGRatio'],DataMain['BookValue'],DataMain['DividendPerShare'],DataMain['DividendYield'],DataMain['EPS'],DataMain['RevenuePerShareTTM'],DataMain['ProfitMargin'],DataMain['OperatingMarginTTM'],DataMain['ReturnOnAssetsTTM'],DataMain['ReturnOnEquityTTM'],DataMain['RevenueTTM'],DataMain['GrossProfitTTM'],DataMain['DilutedEPSTTM'],DataMain['QuarterlyEarningsGrowthYOY'],DataMain['QuarterlyRevenueGrowthYOY'],DataMain['AnalystTargetPrice'],DataMain['TrailingPE'],DataMain['ForwardPE'],DataMain['PriceToSalesRatioTTM'],DataMain['PriceToBookRatio'],DataMain['EVToRevenue'],DataMain['EVToEBITDA'],DataMain['Beta'],DataMain['52WeekHigh'],DataMain['52WeekLow'],DataMain['50DayMovingAverage'],DataMain['200DayMovingAverage'],DataMain['SharesOutstanding'],DataMain['DividendDate'],DataMain['ExDividendDate']))
 
-#print(cur.fetchall())
 
 conn.commit()
-
 conn.close()
 
-#print(test)
-
-#print(DataIncomeStatement['annualReports'][0]['fiscalDateEnding'])
-
-#for item in DataIncomeStatement['annualReports']:
-#    print('                 ')
-#    print(len(item))
-#    print('                 ')
-#    for key, value in item.items():
-#        print(key,value)
